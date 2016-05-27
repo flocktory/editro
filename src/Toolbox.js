00e6adf9536bd9d739be9a1b9f75bc1e0b2ad174
piumosso <golikoffartem@gmail.com>
2016-05-27 14:38:42 +0300

    toolbox class

--- src/Toolbox.js
+++ src/Toolbox.js
@@ -1,45 +1,65 @@
 export default class Toolbox {
-  constructor(el, components, root) {
-    this.rootDomNode = rootDomNode;
-    this.controllers = this.getControllers(el, register);
-    this.render(rootDomNode);
+  constructor(el, controllers, root) {
+    this.root = root;
+    this.controllers = this.getControllers(el, controllers);
+    this.render();
   }
 
-  getControllers(el, register) {
-    const controllers = Object
-      .getOwnPropertyNames(register.controllers)
-      .map(name => register.controllers[name]);
-
+  getControllers(el, controllers) {
     return controllers
       .filter(Controller => Controller.test(el))
-      .map(Controller => new Controller(el));
+      .map(Controller => Controller.create(el));
   }
 
   render() {
-    const controllerGroups = this.getControllerGroups(this.controllers);
-    // ...
-    Object
-      .getOwnPropertyNames(controllerGroups)
-      .forEach(key => {
-        // ...
-        controllerGroups[key].forEach(controller => groupRoot.appendChild(controller.getComponentNode()))
-      })
-    // ...
+    const panel = document.createRange().createContextualFragment(`<section class="EditroPanel"></section>`);
+
+    // Render groups
+    this.getControllerGroups(this.controllers).forEach(controllers => {
+      const group = document.createRange().createContextualFragment(`<div class="EditroPanel-section"></div>`);
+
+      // Render components in group
+      controllers.forEach(controller => {
+        if (!controller.node) {
+          return;
+        }
+
+        const form = document.createRange().createContextualFragment(`<article class="EditroForm">
+          <div class="EditroForm-title">${controller.title}</div>
+          <div class="EditroForm-controls" editoro-controls></div>
+        </article>`);
+
+        form.querySelector('[editoro-controls]').appendChild(controller.node);
+        group.appendChild(form);
+      });
+
+      panel.appendChild(group);
+    });
+
+    this.root.appendChild(panel);
   }
 
   getControllerGroups(controllers) {
-    const controllerGroups = {
-      priority: [],
-      basic: []
-    };
+    const controllerGroups = {};
+    const basicGroup = 'basic';
 
-    controllers.forEach(controller => controllerGroups[controller.getGroup()].push(controller));
+    controllers.forEach(controller => {
+      const group = controller.group || basicGroup;
 
-    return controllerGroups;
+      controllerGroups[group] = controllerGroups[group] || {};
+      controllerGroups[group].push(controller)
+    });
+
+    // Basic group at the end of list
+    return Object
+      .getOwnPropertyNames(controllerGroups)
+      .map(key => [key, controllerGroups[key]])
+      .sort((a, b) => a[0] === basicGroup && b[0] !== basicGroup ? 1 : 0)
+      .map(item => item[1]);
   }
 
   destroy() {
     this.controllers.forEach(controller => controller.destroy());
-    this.rootDomNode.innerHTML = '';
+    this.root.innerHTML = '';
   }
 }
